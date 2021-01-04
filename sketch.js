14216ea6f0a94bd37bb8876a2bec3b601f25f7f9
KashyapPatel1574 <74296198+KashyapPatel1574@users.noreply.github.com>
2021-01-04 12:06:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,78 +1,148 @@
-var border1,border2,border3,border4;
-var rect,rect1,rect2,rect3,rect4,rect5;
-var ball1,ball2,ball3,ball4,ball5,ball6,ball7,ball8;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var stand1, stand1, ground1;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16, box17, box18;
+var polygon1, slingShot;
+var score = 0;
+var backgroundImg;
+var gameState = "on slingshot";
+var bg = "Sprites/bg.jpg";
+
+function preload() {
+  getBackgroundImg();
+  backgroundImg = loadImage("Sprites/bg.jpg");
+}
 
 function setup() {
-  createCanvas(400,500);
- 
-  rect = createSprite(200, 493, 400, 7);
-  rect.shapeColor = "white"
-  rect1 = createSprite(1, 450, 20, 400);
-  rect1.shapeColor = "white"
-  rect2 = createSprite(400, 450, 20, 400);
-  rect2.shapeColor = "white"
-  rect3 = createSprite(100, 450, 5, 400);
-  rect3.shapeColor = "white"
-  rect4 = createSprite(200, 450, 5, 400);
-  rect4.shapeColor = "white"
-  rect5 = createSprite(300, 450, 5, 400);
-  rect5.shapeColor = "white"
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
 
-  border1 = createSprite(399, 200, 10, 600);
-  border1.shapeColor = "brown"
-  border2 = createSprite(1, 200, 10, 600);
-  border2.shapeColor = "brown"
-  border3 = createSprite(200, 1, 400, 10);
-  border3.shapeColor = "brown"
-  border4 = createSprite(200, 499, 400, 10);
-  border4.shapeColor = "brown"
+  createSprite(400, 200, 50, 50);
 
-ball1 = createSprite(30,20,20,20)
-ball1.shapeColor = "pink"
+  stand1 = new Ground(390,height - 100,200,20);
+  stand2 = new Ground(650,height - 200,200,20);
+  ground1 = new Ground(400,height + 50,800,20);
 
-ball2 = createSprite(130,20,20,20)
-ball2.shapeColor = "cyan"
+  box1 = new Box(330, 235, 30, 40);
+  box2 = new Box(360, 235, 30, 40);
+  box3 = new Box(390, 235, 30, 40);
+  box4 = new Box(420, 235, 30, 40);
+  box5 = new Box(450, 235, 30, 40);
 
-ball3 = createSprite(230,20,20,20)
-ball3.shapeColor = "purple"
+  box6 = new Box(360, 195, 30, 40);
+  box7 = new Box(390, 195, 30, 40);
+  box8 = new Box(420, 195, 30, 40);
 
-ball4 = createSprite(330,20,20,20)
-ball4.shapeColor = "orange"
+  box9 = new Box(390, 185, 30, 40);
 
-ball5 = createSprite(60,50,20,20)
-ball5.shapeColor = "blue"
+  box10 = new Box(590, 135, 30, 40);
+  box11 = new Box(620, 135, 30, 40);
+  box12 = new Box(650, 135, 30, 40);
+  box13 = new Box(680, 135, 30, 40);
+  box14 = new Box(710, 135, 30, 40);
 
-ball6 = createSprite(160,50,20,20)
-ball6.shapeColor = "red"
+  box15 = new Box(620, 85, 30, 40);
+  box16 = new Box(650, 85, 30, 40);
+  box17 = new Box(680, 85, 30, 40);
 
-ball7 = createSprite(260,50,20,20)
-ball7.shapeColor = "yellow"
+  box18 = new Box(650, 55, 30, 40);
 
-ball8 = createSprite(360,50,20,20)
-ball8.shapeColor = "green"
+  polygon1 = new Polygon(100, 280, 40);
 
+  slingShot = new SlingShot(polygon1.body, {x: 150, y: 150});
 }
 
 function draw() {
-  ball1.velocityY = 2
-  ball2.velocityY = 2
-  ball3.velocityY = 2
-  ball4.velocityY = 2
-  ball5.velocityY = 3
-  ball6.velocityY = 3
-  ball7.velocityY = 3
-  ball8.velocityY = 3
+  if(backgroundImg) {
+    background(backgroundImg);
+}
+  
+  noStroke();
+  textSize(20);
+  fill("white");
+  text("SCORE :" + score, 650, 40);
+  text("Press the space key for another chance!", 400, 380);
 
-ball1.collide(rect,rect1,rect2,rect3,rect4,rect5,)
-ball2.collide(rect,rect1,rect2,rect3,rect4,rect5)
-ball3.collide(rect,rect1,rect2,rect3,rect4,rect5)
-ball4.collide(rect,rect1,rect2,rect3,rect4,rect5)
-ball5.collide(rect,rect1,rect2,rect3,rect4,rect5,ball1,ball2,ball3,ball4)
-ball6.collide(rect,rect1,rect2,rect3,rect4,rect5,ball1,ball2,ball3,ball4)
-ball7.collide(rect,rect1,rect2,rect3,rect4,rect5,ball1,ball2,ball3,ball4)
-ball8.collide(rect,rect1,rect2,rect3,rect4,rect5,ball1,ball2,ball3,ball4)
+  Engine.update(engine);
 
+  stroke("black");
 
-  background("black");  
-  drawSprites();
+  box1.score();
+  box1.display();
+  box2.score();
+  box2.display();
+  box3.score();
+  box3.display();
+  box4.score();
+  box4.display();
+  box5.score();
+  box5.display();
+  box6.score();
+  box6.display();
+  box7.score();
+  box7.display();
+  box8.score();
+  box8.display();
+  box9.score();
+  box9.display();
+  box10.score();
+  box10.display();
+  box11.score();
+  box11.display();
+  box12.score();
+  box12.display();
+  box13.score();
+  box13.display();
+  box14.score();
+  box14.display();
+  box15.score();
+  box15.display();
+  box16.score();
+  box16.display();
+  box17.score();
+  box17.display();
+  box18.score();
+  box18.display();
+  stand1.display();
+  stand2.display()
+  ground1.display();
+  polygon1.display();
+  slingShot.display();
+}
+
+function mouseDragged(){
+  if(gameState!=="launched"){
+    Matter.Body.setPosition(polygon1.body, {x: mouseX , y: mouseY});
+  }
+}
+
+function mouseReleased() {
+  slingShot.fly();
+  gameState = "launched";
+}
+
+function keyPressed() {
+  if(keyCode === 32) {
+      slingShot.attach(polygon1.body);
+      gameState = "on slingshot";
+  }
+}
+
+async function getBackgroundImg() {
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+  var date = responseJSON.datetime;
+  var hour = date.slice(11, 13);
+  if(hour>=06 && hour<=19){
+      bg = "Sprites/bg.jpg"
+  } else {
+      bg = "Sprites/bg1.jpg"
+  }
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
 }
